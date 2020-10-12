3400a34887fa802fc94f0e569511082995c70713
Mosadoluwa-A <magbonyin@gmail.com>
2020-10-12 04:52:11 +0100

    Initial Commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    date = models.DateField(auto_now_add=False)
+
+    def __str__(self):
+        return self.title
